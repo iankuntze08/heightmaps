26ebd440f06c04d6e670d2bfa1997b71c3a7ff30
Ian Kuntze <iankuntze08@gmail.com>
2026-06-29 11:54:33 -0500

    Cleaned up python script

--- scramble.py
+++ scramble.py
@@ -4,9 +4,7 @@ LENGTH = 256
 
 def main():
     r = random.Random()
-    array = []
-    for x in range(LENGTH):
-        array.append(x)
+    array = list(range(LENGTH))
     r.shuffle(array)
     print(array)
 
